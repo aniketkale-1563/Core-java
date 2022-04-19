090b0ee32b9d8f8738890e25e162f5d06cce546d
aniketkale-1563 <99478932+aniketkale-1563@users.noreply.github.com>
2022-04-19 19:04:53 +0530

    Add files via upload

---
+++ Firstpg.java
@@ -0,0 +1,31 @@
+package myproject;
+
+
+
+public class Firstpg 
+{
+	int yearofpurchase;
+	String model;
+	double cost;
+	
+	public static void displ1()
+	{
+		System.out.println("year of purchasee"+2002);
+		System.out.println("Make:-"+"maruti");
+	}
+	public static void displ2() {
+		System.out.println("Model:-"+1999);
+		System.out.println("cost:-"+500000);
+	}
+
+public static void main(String[] args) 
+	{
+		// TODO Auto-generated method stub
+		Firstpg car=new Firstpg();
+		car.displ1();
+		car.displ2();
+
+	}
+}
+
+
