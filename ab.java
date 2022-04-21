16aa9561febf182601918d73a0c3305b04e70606
aniketkale-1563 <99478932+aniketkale-1563@users.noreply.github.com>
2022-04-22 01:07:53 +0530

    Add files via upload
    Packages in java

---
+++ ab.java
@@ -0,0 +1,9 @@
+package hai;
+
+public class ab 
+{
+ public void Dis() 
+ {
+   System.out.println("Welcome");	
+ }
+}
---
+++ cd.java
@@ -0,0 +1,15 @@
+package by;
+import hai.*;
+
+public class cd 
+{
+
+	public static void main(String[] args) 
+	{
+		// TODO Auto-generated method stub
+           ab ob1=new ab();
+           ob1.Dis();
+           
+	}
+
+}
