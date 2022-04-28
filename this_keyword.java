e20c5077e2aaf88b944598ca3103d07ffdde9ea2
aniketkale-1563 <99478932+aniketkale-1563@users.noreply.github.com>
2022-04-28 16:25:30 +0530

    Add files via upload
    This keyword

---
+++ this_keyword.java
@@ -0,0 +1,15 @@
+package myproject;
+
+public class this_keyword 
+{
+	int age;
+	this_keyword()
+	{
+		this.age=age;
+	}
+	public static void main(String[] args) 
+	{
+		this_keyword ob=new this_keyword();
+		System.out.println("Age Is :-"+ob.age);
+	}
+}
